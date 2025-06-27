ecde2093f1f5eae534f0e5a32aeb1904c3101202
lucasmoraist <luksmnt1101@gmail.com>
2025-06-27 20:29:51 -0300

    feat: update t_event_address table to include zip_code and make number optional

--- src/main/resources/db/migration/V4__create-table-address.sql
+++ src/main/resources/db/migration/V4__create-table-address.sql
@@ -1,8 +1,9 @@
 CREATE TABLE t_event_address (
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     neighborhood_id UUID NOT NULL,
+    zip_code VARCHAR(10) NOT NULL,
     address VARCHAR(255) NOT NULL,
-    number VARCHAR(10) NOT NULL,
+    number VARCHAR(10),
     complement VARCHAR(120),
     CONSTRAINT fk_address_street FOREIGN KEY (neighborhood_id)
         REFERENCES t_event_neighborhood (id)
